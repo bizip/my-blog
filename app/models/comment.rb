f29ab6cb6d3ffce6d59567dbf692482a40ee9da6
bizip <bizip04@gmail.com>
2022-08-17 17:38:23 +0200

    Working on feedbacks

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
 
+  after_save :update_comment_counter
+
+  private
+
   def update_comment_counter
-    posts.increment!(:commentsCounter)
+    author.increment!(:commentsCounter)
   end
 end
