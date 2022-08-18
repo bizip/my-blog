9b64b812052b434d02c2cc1e1f6efb6598e4de20
bizip <bizip04@gmail.com>
2022-08-18 14:51:05 +0200

    install and consigure rspec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, length: { maximum: 250, too_long: "%{count} characters for title is the maximum allowed" }
+  validates :commentsCounter, :numericality => { greater_than_or_equal_to: 0 }
+  validates :likesCounter, :numericality => { greater_than_or_equal_to: 0 }
+
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :postsCountter, :numericality => { greater_than_or_equal_to: 0 }
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
