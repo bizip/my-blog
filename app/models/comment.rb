a648d8ffde27db8ccca0ebc9e44b68421c9a9c1e
Bizimungu Pascal <bizip04@gmail.com>
2022-08-20 23:48:22 +0200

    Working on feedbacks

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :post, counter_cache: true
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  validates :title, presence: true, length: { in: 4..250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_comments_on_counter
     post.increment!(:comments_count)
