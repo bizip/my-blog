774a9576efd067cd6936a8ac2142ae6dd444a00e
Bizimungu Pascal <bizip04@gmail.com>
2022-08-20 23:54:02 +0200

    Fix tthe mistake

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
+  validates :title, presence: true, length: { in: 4..250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   has_many :likes
   has_many :comments
 
