5dbb2bf5c4d3d489213a2307c2e5425f02f72956
bizip <bizip04@gmail.com>
2022-08-23 11:29:28 +0200

    Fix Post id issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
-
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
-  validates :title, presence: true, length: { in: 4..250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   has_many :likes
   has_many :comments
 
@@ -16,3 +12,4 @@ class Post < ApplicationRecord
     author.increment!(:posts_count)
   end
 end
+
