3b5f50c7e0645a6e3e35adabb259d8f320f6469d
bizip <bizip04@gmail.com>
2022-08-24 16:24:58 +0200

    Fix the feedback in user file

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -1,32 +1,39 @@
 require 'rails_helper'
-
-RSpec.describe PostsController, type: :request do
-  before(:each) do
-    @user = User.create(
-      name: 'Pascal',
-      photo: 'https://images.unsplash.com/photo-1660911295696-9810c266d9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG
-      90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-      bio: 'Bacon ipsum dolor amet pork loin chislic porchetta, biltong bresaola jerky bacon
-      tail swine pancetta ham prosciutto beef. Frankfurter burgdoggen corned beef buffalo
-      landjaeger meatloaf
-       chuck kevin sausage shankle meatball. Tongue frankfurter capicola hamburger. Picanha'
-    )
-    @post = Post.create(author: @user, title: 'Pascals story', text: 'this is my future')
+RSpec.describe 'Posts', type: :request do
+  let!(:user) { User.create(name: 'Pascal',
+  photo: 'https://images.unsplash.com/photo-1660911295696-9810
+  c266d9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+  bio: 'Bacon ipsum dolor amet pork loin chislic porchetta, biltong bresaola jerky bacon tail swine pancetta
+  ham prosciutto beef. Frankfurter burgdoggen corned beef buffalo
+    landjaeger meatloaf
+     chuck kevin sausage shankle meatball. Tongue frankfurter capicola hamburger. Picanha') }
+  let!(:post) do
+    Post.create(author: user, title: 'How to eat fish', text: 'you open your moth slowly, this is a lie')
   end
 
-  describe 'GET index Pages' do
-    it 'It should render index page' do
-      get :index, params: { user_id: @user.id }
-      expect(response.status).to eq(200)
-      expect(response).to render_template('index')
+  context 'when GET /index' do
+    before { @posts = post }
+    before(:example) { get user_posts_path(user.id) }
+
+    it 'should have success code of ok' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "should render the 'index' template" do
+      expect(response).to render_template(:index)
     end
   end
+  context 'when GET /show' do
+    before(:example) { get user_post_path(user, post) }
 
-  describe 'GET show Pasges' do
-    it 'It should renders show page' do
-      get :show, params: { id: @post.id, user_id: @user.id }
-      expect(response.status).to eq(200)
-      expect(response).to render_template('show')
+    it 'should have success code of ok' do
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include("you open your moth slowly, this is a lie")
+    end
+
+    it "should render the 'show' template" do
+      expect(response).to render_template(:show)
+      expect(response.body).to include("you open your moth slowly, this is a lie")
     end
   end
 end
