9e042404cdb7f5b6e178d648a175417f603ae900
bizip <bizip04@gmail.com>
2022-08-26 21:50:22 +0200

    Allow user tto add likes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,11 @@
+class LikesController < ApplicationController
+        def create
+        post = Post.find(params[:post_id])
+        author = CurrentUser.user
+        liked = Like.where(author:, post:)
+        return if liked.present?
+        like = Like.create(author:, post:)
+        return unless like.save
+        redirect_to user_post_path(post.author, post)
+        end
+  end
