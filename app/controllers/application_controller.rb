f9a5725905f3377abec98aa9927e58bdd70595b6
bizip <bizip04@gmail.com>
2022-08-26 12:07:48 +0200

    Add the functionality to the form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base 
+    before_action :set_current_user
+
+    def set_current_user
+        CurrentUser.user = User.first
+    end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,29 @@ class PostsController < ApplicationController
     @posts = @user.posts.includes(:comments)
   end
 
-  def new; end
+  def new
+    @post = Post.new
+  end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def create
+    @user = CurrentUser.user
+    @post = Post.create(post_params_from_input)
+    @post.author = @user
+    if @post.save
+      flash[:notice] = 'New post created'
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def post_params_from_input
+    params.require(:post).permit(:title, :text)
   end
 end
