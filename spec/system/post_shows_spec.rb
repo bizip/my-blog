c3085badccbcc51b3c6966c08aad80a0bbabb9c7
bizip <bizip04@gmail.com>
2022-08-30 07:26:58 +0200

    Some test were passing

---
+++ spec/system/post_shows_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe 'PostShows Spec', type: :system do
+  before do
+    driven_by(:rack_test)
+    @user1 = User.create(name: 'Pascal', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @user2 = User.create(name: 'Bizimungu', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @post = Post.create(title: 'Hello', text: 'This is my first post', author: @user1)
+
+    Comment.create(author: @user1, post: @post, text: 'First comment from my mind')
+    Comment.create(author: @user2, post: @post, text: 'Second comment from my mind')
+
+    Like.create(post: @post, author: @user1)
+  end
+
+  it 'It should shows a post\'s title' do
+    visit user_post_path(@user1, @post)
+    expect(page).to have_content(@post.title)
+  end
+
+  it 'It should shows the author of tthe post' do
+    visit user_post_path(@user1, @post)
+    expect(page).to have_content(@user1.name)
+  end
+
+#   it 'shows the number of comments' do
+#     visit user_post_path(@user, @post)
+#     expect(page).to have_content("Comments: #{@post.comments_counter}")
+#   end
+
+#   it 'shows the number of likes' do
+#     visit user_post_path(@user, @post)
+#     expect(page).to have_content("Likes: #{@post.likes_counter}")
+#   end
+
+  it 'It should shows tthe body of the post' do
+    visit user_post_path(@user1, @post)
+    expect(page).to have_content(@post.text)
+  end
+
+  it 'It should describe the user name of each comment' do
+    visit user_post_path(@user1, @post)
+    comment = @post.comments.first
+    expect(page).to have_content(comment.author.name)
+  end
+
+  it 'shows the comment each comment author left' do
+    visit user_post_path(@user1, @post)
+    comment = @post.comments.first
+    comment2 = @post.comments.last
+    expect(page).to have_content(comment.text)
+    expect(page).to have_content(comment2.text)
+  end
+end
