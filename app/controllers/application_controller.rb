2ec7e510247ee07c4b289bc594c26315b4fd5e44
bizip <bizip04@gmail.com>
2022-09-02 23:30:56 +0200

    Configured JWT in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
 
+  respond_to :json, :html
+
+  protect_from_forgery unless: -> { request.format.json? }
+
+  before_action :authenticate_user!
   before_action :update_default_field, if: :devise_controller?
+  rescue_from CanCan::AccessDenied do |exception|
+
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_back fallback_location: root_path, notice: exception.message }
+      format.js { head :forbidden, content_type: 'text/html' }
+    end
+  end
 
   protected
-
   def update_default_field
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password) }
